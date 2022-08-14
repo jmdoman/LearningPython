f094d4f6238114be8b98daa6745a4a1431a683e0
John Doman <jmdoman@protonmail.com>
2022-08-14 11:17:56 -0400

    dom

--- dominic/sleeping!2.py
+++ dominic/sleeping!2.py
@@ -1,11 +1,15 @@
 import random
 import string
-adjectives = ['sleepy', 'slow', 'wet']
-nouns = ['apple', 'ball', 'card']
-adjective = random.choice(adjectives)
-noun = random.choice(nouns)
-number = random.randrange(0, 100)
-goof = random.choice(string.punctuation)
 print('Welcome to Password Picker!')
-password = adjective + noun + str(number) + goof
-print('Your new password is: %s' % password)
+while True:
+    adjectives = ['sleepy', 'slow', 'wet', 'bad', 'fast', 'quick', 'little']
+    nouns = ['apple', 'ball', 'card', 'cat', 'goat', 'book', 'frog', 'dinosaur']
+    adjective = random.choice(adjectives)
+    noun = random.choice(nouns)
+    number = random.randrange(0, 100)
+    goof = random.choice(string.punctuation)
+    password = adjective + noun + str(number) + goof
+    print('Your new password is: %s' % password)
+    response = input('would you like a new password? y/n ')
+    if response == 'n':
+        break
